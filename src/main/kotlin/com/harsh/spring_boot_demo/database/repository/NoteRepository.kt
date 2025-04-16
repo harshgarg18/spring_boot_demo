647428307b4217ac782c79c6858f3ab7c6f37fce
Harsh Garg <harshgarg.1812@gmail.com>
2025-04-16 21:13:57 +0530

    added few endpoints

---
+++ src/main/kotlin/com/harsh/spring_boot_demo/database/repository/NoteRepository.kt
@@ -0,0 +1,9 @@
+package com.harsh.spring_boot_demo.database.repository
+
+import com.harsh.spring_boot_demo.database.model.Note
+import org.bson.types.ObjectId
+import org.springframework.data.mongodb.repository.MongoRepository
+
+interface NoteRepository: MongoRepository<Note, ObjectId> {
+    fun findByOwnerId(ownerId: ObjectId): List<Note>
+}
